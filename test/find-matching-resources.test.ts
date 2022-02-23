32cd1c54a0304d4e9a2b892a19ec885c47571fd8
Josh Kellendonk <joshkellendonk@gmail.com>
2022-02-23 15:42:02 -0700

    remove extra describe

--- test/find-matching-resources.test.ts
+++ test/find-matching-resources.test.ts
@@ -163,40 +163,38 @@ describe('findMatchingResources', () => {
   });
 });
 
-describe('MatchBase', () => {
-  test('comma metadata separators', () => {
-    // GIVEN
-    const spec = ['first=a,second'];
+test('comma metadata separators', () => {
+  // GIVEN
+  const spec = ['first=a,second'];
 
-    // WHEN
-    const metadata = new MetadataMatch(spec);
-    const result = metadata.matches({
-      first: 'a',
-      second: Math.random().toString(),
-    });
-
-    // THEN
-    expect(result).toEqual(result);
+  // WHEN
+  const metadata = new MetadataMatch(spec);
+  const result = metadata.matches({
+    first: 'a',
+    second: Math.random().toString(),
   });
 
-  test('comma tag separators', () => {
-    // GIVEN
-    const spec = ['first=a,second'];
+  // THEN
+  expect(result).toEqual(result);
+});
 
-    // WHEN
-    const tagsMatch = new TagsMatch(spec);
-    const result = tagsMatch.matches([
-      {
-        Key: 'first',
-        Value: 'a',
-      },
-      {
-        Key: 'second',
-        Value: Math.random().toString(),
-      },
-    ]);
+test('comma tag separators', () => {
+  // GIVEN
+  const spec = ['first=a,second'];
 
-    // THEN
-    expect(result).toEqual(true);
-  });
+  // WHEN
+  const tagsMatch = new TagsMatch(spec);
+  const result = tagsMatch.matches([
+    {
+      Key: 'first',
+      Value: 'a',
+    },
+    {
+      Key: 'second',
+      Value: Math.random().toString(),
+    },
+  ]);
+
+  // THEN
+  expect(result).toEqual(true);
 });
